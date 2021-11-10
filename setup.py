e699a0829b96e89fb634442d7900c64ad511b305
Heegyu Kim <heekue83@gmail.com>
2021-11-10 15:13:20 +0900

    requirements가 제대로 안따라가는 문제 수정

--- setup.py
+++ setup.py
@@ -2,18 +2,23 @@ import setuptools
 
 setuptools.setup(
     name="symspellpy_ko",
-    version="0.1.1",
+    version="0.1.4",
     license='GNU General Public License v3 (GPLv3)',
     author="Heegyu Kim",
     author_email="heekue83@gmail.com",
     description="symspellpy를 한글 특성에 맞춰서 수정한 라이브러리. 자소분해를 이용해 더 정확한 오타교정을 해준다.",
     long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     url="https://github.com/HeegyuKim/symspellpy-ko",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['tests']),
+
+    install_requires=open("requirements.txt").readlines(),
+
     include_package_data=True,
     package_data = {
         '': ["*.txt"]
     },
+
     classifiers=[
         # 패키지에 대한 태그
         "Programming Language :: Python :: 3",
